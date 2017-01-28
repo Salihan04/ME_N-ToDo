5d34a9aec2b53b2f1009591e3543af1093fce16b
Salihan <salihan04@gmail.com>
2017-01-28 11:49:45 +0800

    Add main function to listen to a port and start the app

--- app/src/js/server.js
+++ app/src/js/server.js
@@ -1,6 +1,21 @@
+/**
+ * This module is to handle any backend tasks and is the main entry point
+ */
+
 var express = require('express')
 var bodyParser = require('body-parser')
 var methodOverride = require('method-override')
-var config = require('./config')
+var config = require('../../config/config')
 
 var app = express()
+
+/**
+ * Main function to put everything together
+ */
+function main () {
+  app.listen(config.port, () => {
+    console.log('App started...')
+  })
+}
+
+main()
